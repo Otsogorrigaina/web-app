0534a3904f1eb32cb02a70fdba103f23dff5eeb0
jonabasque <jonmultimedia@gmail.com>
2016-09-07 23:30:36 +0200

    Mostrado template 'panels' solo en estado 'app' en ruta /

--- imports/ui/components/panels/panels.js
+++ imports/ui/components/panels/panels.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
 
 import template from './panels.html';
 
@@ -17,11 +18,21 @@ class Panels {
 
 // create a module with a componente
 export default angular.module( name, [
-  //uiRouter,
+  uiRouter,
   angularMeteor ])
     .component(name, {
       templateUrl: template,
       controller: Panels,
       controllerAs: name
     }
-);
+)
+.config(config);
+
+function config($stateProvider) {
+'ngInject';
+$stateProvider
+  .state('app', {
+    url: '/',
+    template: '<panels></panels>'
+  });
+}
