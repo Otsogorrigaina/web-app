20b391e355d38ba1e44a9df7b823d9b66ede7dff
jonabasque <jonmultimedia@gmail.com>
2016-09-15 16:44:33 +0200

    Cambiado codePiwik a componente

--- client/main.js
+++ client/main.js
@@ -4,10 +4,12 @@ import angularMeteor from 'angular-meteor';
 import { name as webMain } from '../imports/ui/components/webMain/webMain';
 import { name as webHeader } from '../imports/ui/components/webHeader/webHeader';
 import { name as webFooter } from '../imports/ui/components/webFooter/webFooter';
+import { name as CodePiwik } from '../imports/ui/components/codePiwik/codePiwik';
 
 angular.module('otsogorriweb-app', [
   angularMeteor,
   webMain,
   webHeader,
-  webFooter
+  webFooter,
+  CodePiwik
 ]);
